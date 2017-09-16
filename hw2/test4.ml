93d17e4b7578667cd0671f1f703fb4a7a9a1bdc8
Jhuni0123 <whdgnsdl887@naver.com>
2017-09-17 03:44:37 +0900

    Add testcases for 2-4

--- hw2/test4.ml
+++ hw2/test4.ml
@@ -14,7 +14,23 @@ module TestEx4: TestEx =
       | CHECK (m, b) -> checkMetro m = b
 
     let testcases =
-      [
+      [ CHECK (AREA ("a", STATION "a"), true)
+      ; CHECK (AREA ("a", AREA ("a", STATION "a")), true)
+      ; CHECK (AREA ("a", AREA ("b", CONNECT (STATION "a", STATION "b"))), true)
+      ; CHECK (AREA ("a", CONNECT (STATION "a", AREA ("b", STATION "a"))), true)
+      ; CHECK (AREA ("a", STATION "b"), false)
+      ; CHECK (AREA ("a", CONNECT (STATION "a", AREA ("b", STATION "c"))), false)
+      ; CHECK (AREA ("a", AREA ("b", CONNECT (STATION "a", STATION "c"))), false)
+      ; CHECK (STATION "a", false)
+      ; CHECK (STATION "b", false)
+      ; CHECK (AREA ("b", STATION "b"), true)
+      ; CHECK (AREA ("asdf", STATION "asdf"), true)
+      ; CHECK (AREA ("b", STATION "a"), false)
+      ; CHECK (AREA ("seoul", STATION "busan"), false)
+      ; CHECK (CONNECT (AREA ("seoul", STATION "seoul"), AREA ("busan", STATION "busan")), true)
+      ; CHECK (CONNECT (AREA ("seoul", STATION "seoul"), AREA ("busan", STATION "seoul")), false)
+      ; CHECK (CONNECT (AREA ("seoul", STATION "busan"), AREA ("busan", STATION "busan")), false)
+      ; CHECK (CONNECT (AREA ("seoul", STATION "busan"), AREA ("busan", STATION "seoul")), false)
       ]
   end
 
