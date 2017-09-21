0a5cb5cd3977dc97ba259a937fd659cc97f8389c
Jhuni0123 <whdgnsdl887@naver.com>
2017-09-21 19:06:30 +0900

    Use shell script instead of makefile

---
+++ hw1/check.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+HWNUM=1
+EXERCISES=(1 2 3 4)
+LIB_PATH="../lib/"
+LIBSRC="testlib.ml"
+
+if [[ $# -eq 0 ]]
+then
+    for EX in "${EXERCISES[@]}"; do
+        SRCS+=("ex${EX}.ml")
+        SRCS+=("test${EX}.ml")
+    done
+    echo "# Compiling Every Exercises"
+    ocamlc -I "${LIB_PATH}" -o hw${HWNUM}.out ${LIB_PATH}${LIBSRC} "${SRCS[@]}" \
+        && ./hw${HWNUM}.out total
+elif [[ $# -eq 1 ]]
+then
+    VALID=0
+    for EX in "${EXERCISES[@]}"; do
+        if [[ $1 -eq ${EX} ]]
+        then
+            VALID=1
+            break
+        fi
+    done
+    if [[ ${VALID} -eq 1 ]]
+    then
+        EX=$1
+        echo "# Compiling Exercise ${EX}"
+        ocamlc -I "${LIB_PATH}" -o ex"${EX}".out ${LIB_PATH}${LIBSRC} ex"${EX}".ml test"${EX}".ml \
+            && ./ex"${EX}".out
+    else
+        echo "Error: Exercise ${1} not found"
+    fi
+else
+    echo "Error: Too many arguments"
+fi
